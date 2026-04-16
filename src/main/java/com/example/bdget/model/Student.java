1e53de6174fa9939c741d28bd2e0db9739c2d46f
cvalverd <cvalverd.os@gmail.com>
2026-04-15 21:08:53 -0400

    controller advisor

--- src/main/java/com/example/bdget/model/Student.java
+++ src/main/java/com/example/bdget/model/Student.java
@@ -1,11 +1,12 @@
 package com.example.bdget.model;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import jakarta.validation.constraints.*;
 @Entity
 @Table(name = "student")
 public class Student {
@@ -13,6 +14,10 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "El nombre solo puede contener letras")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
+    @NotBlank(message = "No puede ingresar un nombre vacio")
     @Column(name= "name")
     private String name;
 
